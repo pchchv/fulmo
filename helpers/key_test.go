f0b9efd2e0c88f7fd2a4f3683ba3e6af5767eca8
pchchv <ipchchv@gmail.com>
2025-11-21 21:23:05 +0300

    tests(helpers): add test of KeyToHash function

--- helpers/key_test.go
+++ helpers/key_test.go
@@ -1,10 +1,40 @@
 package helpers
 
 import (
+	"math"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 )
+
+func TestKeyToHash(t *testing.T) {
+	var key uint64
+	var conflict uint64
+
+	key, conflict = KeyToHash(uint64(1))
+	verifyHashProduct(t, 1, 0, key, conflict)
+
+	key, conflict = KeyToHash(uint(1))
+	verifyHashProduct(t, 1, 0, key, conflict)
+
+	key, conflict = KeyToHash(1)
+	verifyHashProduct(t, 1, 0, key, conflict)
+
+	key, conflict = KeyToHash(int32(2))
+	verifyHashProduct(t, 2, 0, key, conflict)
+
+	key, conflict = KeyToHash(int32(-2))
+	verifyHashProduct(t, math.MaxUint64-1, 0, key, conflict)
+
+	key, conflict = KeyToHash(int64(-2))
+	verifyHashProduct(t, math.MaxUint64-1, 0, key, conflict)
+
+	key, conflict = KeyToHash(uint32(3))
+	verifyHashProduct(t, 3, 0, key, conflict)
+
+	key, conflict = KeyToHash(int64(3))
+	verifyHashProduct(t, 3, 0, key, conflict)
+}
 
 func verifyHashProduct(t *testing.T, wantKey, wantConflict, key, conflict uint64) {
 	require.Equal(t, wantKey, key)
