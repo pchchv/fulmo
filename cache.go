d4f2696f68a4235894c705e571518cd0d93ceb80
pchchv <ipchchv@gmail.com>
2025-11-21 09:16:02 +0300

    feat: inplement an Item type

--- cache.go
+++ cache.go
@@ -1,9 +1,22 @@
 package fulmo
 
+import "time"
+
 const (
 	itemNew itemFlag = iota
 	itemDelete
 	itemUpdate
 )
+
+// Item is a full representation of what's stored in the cache for each key-value pair.
+type Item[V any] struct {
+	flag       itemFlag
+	Key        uint64
+	Conflict   uint64
+	Value      V
+	Cost       int64
+	Expiration time.Time
+	wait       chan struct{}
+}
 
 type itemFlag byte
