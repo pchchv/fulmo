38f415e98431812d5cad0a773ff733bc866c316c
pchchv <ipchchv@gmail.com>
2025-11-21 20:58:16 +0300

    feat(helpers): implement MemHash function

--- helpers/rtutil.go
+++ helpers/rtutil.go
@@ -7,6 +7,15 @@ type stringStruct struct {
 	len int
 }
 
+// MemHash is the hash function used by go map,
+// it utilizes available hardware instructions(behaves as aeshash if aes instruction is available).
+// NOTE: The hash seed changes for every process.
+// So, this cannot be used as a persistent hash.
+func MemHash(data []byte) uint64 {
+	ss := (*stringStruct)(unsafe.Pointer(&data))
+	return uint64(memhash(ss.str, 0, uintptr(ss.len)))
+}
+
 //go:noescape
 //go:linkname memhash runtime.memhash
 func memhash(p unsafe.Pointer, h, s uintptr) uintptr
