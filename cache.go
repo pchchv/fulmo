7bd26cf2dd467a3c395923b302902c892b319d02
pchchv <ipchchv@gmail.com>
2025-11-21 09:20:11 +0300

    feat: implement stringFor function

--- cache.go
+++ cache.go
@@ -6,6 +6,19 @@ const (
 	itemNew itemFlag = iota
 	itemDelete
 	itemUpdate
+
+	hit = iota // keep track of hits and misses
+	miss
+	keyAdd // keep track of number of keys added, updated and evicted
+	keyUpdate
+	keyEvict
+	costAdd // keep track of cost of keys added and evicted
+	costEvict
+	dropSets // keep track of how many sets were dropped or rejected later
+	rejectSets
+	dropGets // keep track of how many gets were kept and dropped on the floor
+	keepGets
+	doNotUse // should be the final enum. Other enums should be set before this
 )
 
 // Item is a full representation of what's stored in the cache for each key-value pair.
@@ -23,3 +36,31 @@ type itemFlag byte
 
 type metricType int
 
+func stringFor(t metricType) string {
+	switch t {
+	case hit:
+		return "hit"
+	case miss:
+		return "miss"
+	case keyAdd:
+		return "keys-added"
+	case keyUpdate:
+		return "keys-updated"
+	case keyEvict:
+		return "keys-evicted"
+	case costAdd:
+		return "cost-added"
+	case costEvict:
+		return "cost-evicted"
+	case dropSets:
+		return "sets-dropped"
+	case rejectSets:
+		return "sets-rejected" // by policy.
+	case dropGets:
+		return "gets-dropped"
+	case keepGets:
+		return "gets-kept"
+	default:
+		return "unidentified"
+	}
+}
