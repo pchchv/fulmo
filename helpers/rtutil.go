1359b498cadeac5289c04dd2d6c0cf9cecdd759b
pchchv <ipchchv@gmail.com>
2025-11-21 20:38:04 +0300

    feat(helpers): Implement stringStruct type

--- helpers/rtutil.go
+++ helpers/rtutil.go
@@ -2,6 +2,11 @@ package helpers
 
 import "unsafe"
 
+type stringStruct struct {
+	str unsafe.Pointer
+	len int
+}
+
 //go:noescape
 //go:linkname memhash runtime.memhash
 func memhash(p unsafe.Pointer, h, s uintptr) uintptr
