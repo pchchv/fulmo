34ecee9cf8466a7e5edc441c453d4ed85feacb90
pchchv <ipchchv@gmail.com>
2025-11-22 22:48:16 +0300

    feat: implement get and cleat methods for cmRow

--- sketch.go
+++ sketch.go
@@ -6,3 +6,14 @@ type cmRow []byte
 func newCmRow(numCounters int64) cmRow {
 	return make(cmRow, numCounters/2)
 }
+
+func (r cmRow) get(n uint64) byte {
+	return (r[n/2] >> ((n & 1) * 4)) & 0x0f
+}
+
+func (r cmRow) clear() {
+	// zero each counter
+	for i := range r {
+		r[i] = 0
+	}
+}
