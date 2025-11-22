7c37a35d319c7077ac5c3854391691b28bc18218
pchchv <ipchchv@gmail.com>
2025-11-22 23:13:43 +0300

    test(helpers): add Bloom test

---
+++ helpers/bloom_test.go
@@ -0,0 +1,26 @@
+package helpers
+
+import (
+	"crypto/rand"
+	"fmt"
+	"testing"
+)
+
+var (
+	wordlist1 [][]byte
+	n         = 1 << 16
+)
+
+func TestMain(m *testing.M) {
+	wordlist1 = make([][]byte, n)
+	for i := range wordlist1 {
+		b := make([]byte, 32)
+		_, _ = rand.Read(b)
+		wordlist1[i] = b
+	}
+
+	fmt.Println("\n###############\nbbloom_test.go")
+	fmt.Print("Benchmarks relate to 2**16 OP. --> output/65536 op/ns\n###############\n\n")
+
+	m.Run()
+}
