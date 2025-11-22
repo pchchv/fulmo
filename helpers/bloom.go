62a5679b965b0d0e648006723325417f64abdc28
pchchv <ipchchv@gmail.com>
2025-11-22 23:15:38 +0300

    feat(helpers): implement Size and TotalSize methods for Bloom

--- helpers/bloom.go
+++ helpers/bloom.go
@@ -1,6 +1,6 @@
 package helpers
 
-// Bloom filter
+// Bloom filter.
 type Bloom struct {
 	bitset  []uint64
 	ElemNum uint64
@@ -8,4 +8,16 @@ type Bloom struct {
 	size    uint64
 	setLocs uint64
 	shift   uint64
+}
+
+// Size makes Bloom filter with as bitset of size sz.
+func (bl *Bloom) Size(sz uint64) {
+	bl.bitset = make([]uint64, sz>>6)
+}
+
+// TotalSize returns the total size of the bloom filter.
+func (bl *Bloom) TotalSize() int {
+	// bl struct has 5 members and each one is 8 byte
+	// bitset is a uint64 byte slice
+	return len(bl.bitset)*8 + 5*8
 }
