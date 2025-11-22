f371eb1bbafbf779e211848cce45d2a24a8fae75
pchchv <ipchchv@gmail.com>
2025-11-22 22:49:40 +0300

    feat: implement reset and string methods for cmRow

--- sketch.go
+++ sketch.go
@@ -1,5 +1,7 @@
 package fulmo
 
+import "fmt"
+
 // cmRow is a row of bytes, with each byte holding two counters.
 type cmRow []byte
 
@@ -16,4 +18,18 @@ func (r cmRow) clear() {
 	for i := range r {
 		r[i] = 0
 	}
+}
+
+func (r cmRow) reset() {
+	// halve each counter
+	for i := range r {
+		r[i] = (r[i] >> 1) & 0x77
+	}
+}
+
+func (r cmRow) string() (s string) {
+	for i := uint64(0); i < uint64(len(r)*2); i++ {
+		s += fmt.Sprintf("%02d ", (r[(i/2)]>>((i&1)*4))&0x0f)
+	}
+	return s[:len(s)-1]
 }
