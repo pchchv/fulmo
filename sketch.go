b1dfcaec8b3b2ed28a00ce0e381e4e3c51b227f8
pchchv <ipchchv@gmail.com>
2025-11-22 22:53:03 +0300

    feat: implement next2Power function

--- sketch.go
+++ sketch.go
@@ -46,3 +46,17 @@ func (r cmRow) increment(n uint64) {
 		r[i] += 1 << s
 	}
 }
+
+// next2Power rounds x up to the next power of 2,
+// if it's not already one.
+func next2Power(x int64) int64 {
+	x--
+	x |= x >> 1
+	x |= x >> 2
+	x |= x >> 4
+	x |= x >> 8
+	x |= x >> 16
+	x |= x >> 32
+	x++
+	return x
+}
