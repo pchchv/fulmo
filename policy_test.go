76555622dd2de09af3162f0437a7c8c01ac69152
pchchv <ipchchv@gmail.com>
2025-11-23 05:33:53 +0300

    test: add tests for Push and clear methods

--- policy_test.go
+++ policy_test.go
@@ -27,3 +27,20 @@ func TestTinyLFUEstimate(t *testing.T) {
 	require.Equal(t, int64(3), a.Estimate(1))
 	require.Equal(t, int64(0), a.Estimate(2))
 }
+
+func TestTinyLFUPush(t *testing.T) {
+	a := newTinyLFU(16)
+	a.Push([]uint64{1, 2, 2, 3, 3, 3})
+	require.Equal(t, int64(1), a.Estimate(1))
+	require.Equal(t, int64(2), a.Estimate(2))
+	require.Equal(t, int64(3), a.Estimate(3))
+	require.Equal(t, int64(6), a.incrs)
+}
+
+func TestTinyLFUClear(t *testing.T) {
+	a := newTinyLFU(16)
+	a.Push([]uint64{1, 3, 3, 3})
+	a.clear()
+	require.Equal(t, int64(0), a.incrs)
+	require.Equal(t, int64(0), a.Estimate(3))
+}
