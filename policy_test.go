ba6ebd75281efa33dfeb1b1f0d11a38d26dccd51
pchchv <ipchchv@gmail.com>
2025-11-23 05:33:22 +0300

    test: add tests for Increment and Estimate methods

---
+++ policy_test.go
@@ -0,0 +1,29 @@
+package fulmo
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestTinyLFUIncrement(t *testing.T) {
+	a := newTinyLFU(4)
+	a.Increment(1)
+	a.Increment(1)
+	a.Increment(1)
+	require.True(t, a.door.Has(1))
+	require.Equal(t, int64(2), a.freq.Estimate(1))
+
+	a.Increment(1)
+	require.False(t, a.door.Has(1))
+	require.Equal(t, int64(1), a.freq.Estimate(1))
+}
+
+func TestTinyLFUEstimate(t *testing.T) {
+	a := newTinyLFU(8)
+	a.Increment(1)
+	a.Increment(1)
+	a.Increment(1)
+	require.Equal(t, int64(3), a.Estimate(1))
+	require.Equal(t, int64(0), a.Estimate(2))
+}
