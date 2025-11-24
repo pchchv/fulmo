201234492a2bd82680c47b0bbcd1be47674552e2
pchchv <ipchchv@gmail.com>
2025-11-24 19:04:30 +0300

    test(metrics): add tests for Ratio and String methods

--- cache_test.go
+++ cache_test.go
@@ -38,3 +38,50 @@ func TestMetricsAddGet(t *testing.T) {
 	m.add(hit, 1, 1)
 	require.Equal(t, uint64(0), m.Hits())
 }
+
+func TestMetricsRatio(t *testing.T) {
+	m := newMetrics()
+	require.Equal(t, float64(0), m.Ratio())
+
+	m.add(hit, 1, 1)
+	m.add(hit, 2, 2)
+	m.add(miss, 1, 1)
+	m.add(miss, 2, 2)
+	require.Equal(t, 0.5, m.Ratio())
+
+	m = nil
+	require.Equal(t, float64(0), m.Ratio())
+}
+
+func TestMetricsString(t *testing.T) {
+	m := newMetrics()
+	m.add(hit, 1, 1)
+	m.add(miss, 1, 1)
+	m.add(keyAdd, 1, 1)
+	m.add(keyUpdate, 1, 1)
+	m.add(keyEvict, 1, 1)
+	m.add(costAdd, 1, 1)
+	m.add(costEvict, 1, 1)
+	m.add(dropSets, 1, 1)
+	m.add(rejectSets, 1, 1)
+	m.add(dropGets, 1, 1)
+	m.add(keepGets, 1, 1)
+	require.Equal(t, uint64(1), m.Hits())
+	require.Equal(t, uint64(1), m.Misses())
+	require.Equal(t, 0.5, m.Ratio())
+	require.Equal(t, uint64(1), m.KeysAdded())
+	require.Equal(t, uint64(1), m.KeysUpdated())
+	require.Equal(t, uint64(1), m.KeysEvicted())
+	require.Equal(t, uint64(1), m.CostAdded())
+	require.Equal(t, uint64(1), m.CostEvicted())
+	require.Equal(t, uint64(1), m.SetsDropped())
+	require.Equal(t, uint64(1), m.SetsRejected())
+	require.Equal(t, uint64(1), m.GetsDropped())
+	require.Equal(t, uint64(1), m.GetsKept())
+
+	require.NotEqual(t, 0, len(m.String()))
+
+	m = nil
+	require.Equal(t, 0, len(m.String()))
+	require.Equal(t, "unidentified", stringFor(doNotUse))
+}
