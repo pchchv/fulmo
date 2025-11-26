ccb0b2ad04e4194df4711ca9960d4f1dbc801ecb
pchchv <ipchchv@gmail.com>
2025-11-26 19:26:58 +0300

    feat: Implement storageBucket function

--- ttl.go
+++ ttl.go
@@ -1,4 +1,13 @@
 package fulmo
 
+import "time"
+
+// TODO: find the optimal value or make it configurable.
+var bucketDurationSecs int64 = 5
+
 // bucket type is a map of key to conflict.
 type bucket map[uint64]uint64
+
+func storageBucket(t time.Time) int64 {
+	return (t.Unix() / bucketDurationSecs) + 1
+}
