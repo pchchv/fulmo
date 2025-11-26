b617a27cb5e15a198fe4c026fdb104ab3c3f3f9a
pchchv <ipchchv@gmail.com>
2025-11-26 19:28:58 +0300

    feat: Implement expirationMap type

--- ttl.go
+++ ttl.go
@@ -1,12 +1,29 @@
 package fulmo
 
-import "time"
+import (
+	"sync"
+	"time"
+)
 
 // TODO: find the optimal value or make it configurable.
 var bucketDurationSecs int64 = 5
 
 // bucket type is a map of key to conflict.
 type bucket map[uint64]uint64
+
+// expirationMap is a map of bucket number to the corresponding bucket.
+type expirationMap[V any] struct {
+	sync.RWMutex
+	buckets              map[int64]bucket
+	lastCleanedBucketNum int64
+}
+
+func newExpirationMap[V any]() *expirationMap[V] {
+	return &expirationMap[V]{
+		buckets:              make(map[int64]bucket),
+		lastCleanedBucketNum: cleanupBucket(time.Now()),
+	}
+}
 
 func storageBucket(t time.Time) int64 {
 	return (t.Unix() / bucketDurationSecs) + 1
