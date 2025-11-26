e77361bb6533ee556d9ecc0ced32f89a8571baed
pchchv <ipchchv@gmail.com>
2025-11-26 19:27:54 +0300

    feat: Implement cleanupBucket function

--- ttl.go
+++ ttl.go
@@ -11,3 +11,10 @@ type bucket map[uint64]uint64
 func storageBucket(t time.Time) int64 {
 	return (t.Unix() / bucketDurationSecs) + 1
 }
+
+func cleanupBucket(t time.Time) int64 {
+	// the bucket to cleanup is always behind the
+	// storage bucket by one so that no elements in that bucket
+	// (which might not have expired yet) are deleted
+	return storageBucket(t) - 1
+}
