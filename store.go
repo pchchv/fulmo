98aa36fe8da3fc066fa2ae12155c058ce059a624
pchchv <ipchchv@gmail.com>
2025-11-27 20:32:48 +0300

    feat: add storeItem type

--- store.go
+++ store.go
@@ -1,3 +1,12 @@
 package fulmo
 
+import "time"
+
 type updateFn[V any] func(cur, prev V) bool
+
+type storeItem[V any] struct {
+	key        uint64
+	value      V
+	conflict   uint64
+	expiration time.Time
+}
