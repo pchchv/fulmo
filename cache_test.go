c698bbed2dc0724808f92d392ebc42db9fe377a5
pchchv <ipchchv@gmail.com>
2025-11-28 22:05:25 +0300

    test: add tests for MaxCost and Metrics SetWithTTL

--- cache_test.go
+++ cache_test.go
@@ -1,6 +1,8 @@
 package fulmo
 
 import (
+	"math/rand"
+	"strings"
 	"testing"
 	"time"
 
@@ -151,6 +153,77 @@ func TestCacheGet(t *testing.T) {
 	val, ok = c.Get(0)
 	require.False(t, ok)
 	require.Zero(t, val)
+}
+
+func TestCacheMaxCost(t *testing.T) {
+	charset := "abcdefghijklmnopqrstuvwxyz0123456789"
+	key := func() []byte {
+		k := make([]byte, 2)
+		for i := range k {
+			k[i] = charset[rand.Intn(len(charset))]
+		}
+		return k
+	}
+	c, err := NewCache(&Config[[]byte, string]{
+		NumCounters: 12960, // 36^2 * 10
+		MaxCost:     1e6,   // 1mb
+		BufferItems: 64,
+		Metrics:     true,
+	})
+	require.NoError(t, err)
+	stop := make(chan struct{}, 8)
+	for i := 0; i < 8; i++ {
+		go func() {
+			for {
+				select {
+				case <-stop:
+					return
+				default:
+					time.Sleep(time.Millisecond)
+
+					k := key()
+					if _, ok := c.Get(k); !ok {
+						val := ""
+						if rand.Intn(100) < 10 {
+							val = "test"
+						} else {
+							val = strings.Repeat("a", 1000)
+						}
+						c.Set(key(), val, int64(2+len(val)))
+					}
+				}
+			}
+		}()
+	}
+
+	for i := 0; i < 20; i++ {
+		time.Sleep(time.Second)
+		cacheCost := c.Metrics.CostAdded() - c.Metrics.CostEvicted()
+		t.Logf("total cache cost: %d\n", cacheCost)
+		require.True(t, float64(cacheCost) <= float64(1e6*1.05))
+	}
+
+	for i := 0; i < 8; i++ {
+		stop <- struct{}{}
+	}
+}
+
+func TestCacheMetrics(t *testing.T) {
+	c, err := NewCache(&Config[int, int]{
+		NumCounters:        100,
+		MaxCost:            10,
+		IgnoreInternalCost: true,
+		BufferItems:        64,
+		Metrics:            true,
+	})
+	require.NoError(t, err)
+
+	for i := 0; i < 10; i++ {
+		c.Set(i, i, 1)
+	}
+	time.Sleep(wait)
+	m := c.Metrics
+	require.Equal(t, uint64(10), m.KeysAdded())
 }
 
 func newTestCache() (*Cache[int, int], error) {
