5005e7024c5c93c0bfd261faada352963a965984
pchchv <ipchchv@gmail.com>
2025-11-28 21:32:54 +0300

    feat: implement Push method for testConsumer

--- ring_test.go
+++ ring_test.go
@@ -4,3 +4,11 @@ type testConsumer struct {
 	push func([]uint64)
 	save bool
 }
+
+func (c *testConsumer) Push(items []uint64) bool {
+	if c.save {
+		c.push(items)
+		return true
+	}
+	return false
+}
