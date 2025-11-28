47b10045bc2ec9a811423bb3743fc654ab3b3d00
pchchv <ipchchv@gmail.com>
2025-11-28 21:41:47 +0300

    feat: add a Config type

--- cache.go
+++ cache.go
@@ -47,6 +47,101 @@ type itemFlag byte
 
 type metricType int
 
+// Config is passed to NewCache for creating new Cache instances.
+type Config[K Key, V any] struct {
+	// NumCounters determines the number of counters (keys) to keep that hold
+	// access frequency information. It's generally a good idea to have more
+	// counters than the max cache capacity, as this will improve eviction
+	// accuracy and subsequent hit ratios.
+	//
+	// For example, if you expect your cache to hold 1,000,000 items when full,
+	// NumCounters should be 10,000,000 (10x). Each counter takes up roughly
+	// 3 bytes (4 bits for each counter * 4 copies plus about a byte per
+	// counter for the bloom filter). Note that the number of counters is
+	// internally rounded up to the nearest power of 2, so the space usage
+	// may be a little larger than 3 bytes * NumCounters.
+	//
+	// Seen good performance in setting this to 10x the number of items
+	// you expect to keep in the cache when full.
+	NumCounters int64
+	// MaxCost is how eviction decisions are made. For example, if MaxCost is
+	// 100 and a new item with a cost of 1 increases total cache cost to 101,
+	// 1 item will be evicted.
+	//
+	// MaxCost can be considered as the cache capacity, in whatever units you
+	// choose to use.
+	//
+	// For example, if you want the cache to have a max capacity of 100MB, you
+	// would set MaxCost to 100,000,000 and pass an item's number of bytes as
+	// the `cost` parameter for calls to Set. If new items are accepted, the
+	// eviction process will take care of making room for the new item and not
+	// overflowing the MaxCost value.
+	//
+	// MaxCost could be anything as long as it matches how you're using the cost
+	// values when calling Set.
+	MaxCost int64
+	// BufferItems determines the size of Get buffers.
+	//
+	// Unless you have a rare use case, using `64` as the BufferItems value
+	// results in good performance.
+	//
+	// If for some reason you see Get performance decreasing with lots of
+	// contention (you shouldn't), try increasing this value in increments of 64.
+	// This is a fine-tuning mechanism and you probably won't have to touch this.
+	BufferItems int64
+	// Metrics is true when you want variety of stats about the cache.
+	// There is some overhead to keeping statistics, so you should only set this
+	// flag to true when testing or throughput performance isn't a major factor.
+	Metrics bool
+	// OnEvict is called for every eviction with the evicted item.
+	OnEvict func(item *Item[V])
+	// OnReject is called for every rejection done via the policy.
+	OnReject func(item *Item[V])
+	// OnExit is called whenever a value is removed from cache. This can be
+	// used to do manual memory deallocation. Would also be called on eviction
+	// as well as on rejection of the value.
+	OnExit func(val V)
+	// ShouldUpdate is called when a value already exists in cache and is being updated.
+	// If ShouldUpdate returns true, the cache continues with the update (Set). If the
+	// function returns false, no changes are made in the cache. If the value doesn't
+	// already exist, the cache continue with setting that value for the given key.
+	//
+	// In this function, you can check whether the new value is valid. For example, if
+	// your value has timestamp assosicated with it, you could check whether the new
+	// value has the latest timestamp, preventing you from setting an older value.
+	ShouldUpdate func(cur, prev V) bool
+	// KeyToHash function is used to customize the key hashing algorithm.
+	// Each key will be hashed using the provided function. If keyToHash value
+	// is not set, the default keyToHash function is used.
+	//
+	// Ristretto has a variety of defaults depending on the underlying interface type
+	// https://github.com/hypermodeinc/ristretto/blob/main/z/z.go#L19-L41).
+	//
+	// Note that if you want 128bit hashes you should use the both the values
+	// in the return of the function. If you want to use 64bit hashes, you can
+	// just return the first uint64 and return 0 for the second uint64.
+	KeyToHash func(key K) (uint64, uint64)
+	// Cost evaluates a value and outputs a corresponding cost. This function is ran
+	// after Set is called for a new item or an item is updated with a cost param of 0.
+	//
+	// Cost is an optional function you can pass to the Config in order to evaluate
+	// item cost at runtime, and only whentthe Set call isn't going to be dropped. This
+	// is useful if calculating item cost is particularly expensive and you don't want to
+	// waste time on items that will be dropped anyways.
+	//
+	// To signal to Ristretto that you'd like to use this Cost function:
+	//   1. Set the Cost field to a non-nil function.
+	//   2. When calling Set for new items or item updates, use a `cost` of 0.
+	Cost func(value V) int64
+	// IgnoreInternalCost set to true indicates to the cache that the cost of
+	// internally storing the value should be ignored. This is useful when the
+	// cost passed to set is not using bytes as units. Keep in mind that setting
+	// this to true will increase the memory usage.
+	IgnoreInternalCost bool
+	// TtlTickerDurationInSec sets the value of time ticker for cleanup keys on TTL expiry.
+	TtlTickerDurationInSec int64
+}
+
 // Metrics is a snapshot of performance statistics for the lifetime of a cache instance.
 type Metrics struct {
 	mu   sync.RWMutex
@@ -221,35 +316,6 @@ func (p *Metrics) trackEviction(numSeconds int64) {
 	p.life.Update(numSeconds)
 }
 
-func stringFor(t metricType) string {
-	switch t {
-	case hit:
-		return "hit"
-	case miss:
-		return "miss"
-	case keyAdd:
-		return "keys-added"
-	case keyUpdate:
-		return "keys-updated"
-	case keyEvict:
-		return "keys-evicted"
-	case costAdd:
-		return "cost-added"
-	case costEvict:
-		return "cost-evicted"
-	case dropSets:
-		return "sets-dropped"
-	case rejectSets:
-		return "sets-rejected" // by policy.
-	case dropGets:
-		return "gets-dropped"
-	case keepGets:
-		return "gets-kept"
-	default:
-		return "unidentified"
-	}
-}
-
 // Cache is a thread-safe hash map implementation with a TinyLFU admission policy and a SampledLFU eviction policy.
 // A single Cache instance can be used by any number of goroutines.
 type Cache[K Key, V any] struct {
@@ -288,6 +354,35 @@ type Cache[K Key, V any] struct {
 	// Metrics contains a running log of important statistics like hits, misses,
 	// and dropped items.
 	Metrics *Metrics
+}
+
+func stringFor(t metricType) string {
+	switch t {
+	case hit:
+		return "hit"
+	case miss:
+		return "miss"
+	case keyAdd:
+		return "keys-added"
+	case keyUpdate:
+		return "keys-updated"
+	case keyEvict:
+		return "keys-evicted"
+	case costAdd:
+		return "cost-added"
+	case costEvict:
+		return "cost-evicted"
+	case dropSets:
+		return "sets-dropped"
+	case rejectSets:
+		return "sets-rejected" // by policy.
+	case dropGets:
+		return "gets-dropped"
+	case keepGets:
+		return "gets-kept"
+	default:
+		return "unidentified"
+	}
 }
 
 func zeroValue[T any]() T {
