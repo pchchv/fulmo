8d1ce8c33425e6e63dd63bba89bf5a8b718776a8
pchchv <ipchchv@gmail.com>
2025-11-28 21:25:05 +0300

    feat: add ringBuffer type

--- ring.go
+++ ring.go
@@ -1,7 +1,18 @@
 package fulmo
 
+import "sync"
+
 // ringConsumer is the user-defined object responsible for
 // receiving and processing items in batches when buffers are drained.
 type ringConsumer interface {
 	Push([]uint64) bool
+}
+
+// ringBuffer stores multiple buffers (stripes)
+// and distributes Pushed items between them to lower contention.
+//
+// This implements the "batching" process described in
+// the BP-Wrapper paper (section III part A).
+type ringBuffer struct {
+	pool *sync.Pool
 }
