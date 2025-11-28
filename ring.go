767d1aa1481c13605ec0c05651ba12f3f5f34b4b
pchchv <ipchchv@gmail.com>
2025-11-28 21:25:48 +0300

    feat: add ringStripe type with constructor

--- ring.go
+++ ring.go
@@ -16,3 +16,18 @@ type ringConsumer interface {
 type ringBuffer struct {
 	pool *sync.Pool
 }
+
+// ringStripe is a singular ring buffer that is not concurrent safe.
+type ringStripe struct {
+	cons ringConsumer
+	data []uint64
+	capa int
+}
+
+func newRingStripe(cons ringConsumer, capa int64) *ringStripe {
+	return &ringStripe{
+		cons: cons,
+		data: make([]uint64, 0, capa),
+		capa: int(capa),
+	}
+}
