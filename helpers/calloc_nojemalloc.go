b20cc024a541c9a5b1f5cea8428798d0c6e1b5d9
pchchv <ipchchv@gmail.com>
2025-11-30 10:56:10 +0300

    feat(helpers): implement providers for Free, Calloc and CallocNoRef

---
+++ helpers/calloc_nojemalloc.go
@@ -0,0 +1,21 @@
+//go:build !jemalloc || !cgo
+// +build !jemalloc !cgo
+
+package helpers
+
+// Provides versions of Calloc, CallocNoRef, etc when jemalloc is not available
+// (eg: build without jemalloc tag).
+
+// Free does not do anything in this mode.
+func Free(b []byte) {}
+
+// Calloc allocates a slice of size n.
+func Calloc(n int, tag string) []byte {
+	return make([]byte, n)
+}
+
+// CallocNoRef will not give you memory back without jemalloc.
+func CallocNoRef(n int, tag string) []byte {
+	// We do the add here just to stay compatible with a corresponding Free call.
+	return nil
+}
