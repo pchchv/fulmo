3af78859291476393ad3738af52694b365ec17a0
pchchv <ipchchv@gmail.com>
2025-11-30 11:17:15 +0300

    feat(sim): implement Simulator type

--- sim/sim.go
+++ sim/sim.go
@@ -1,1 +1,7 @@
 package sim
+
+// Simulator is the core type of the `sim` package.
+// It is a function that returns a key from some source
+// (composed of other functions in this package, generated or parsed).
+// Simulators can be used to approximate access distributions.
+type Simulator func() (uint64, error)
