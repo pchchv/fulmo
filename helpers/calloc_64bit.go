21fe9b4ada5b98b5726e873de150dee05dd0f680
pchchv <ipchchv@gmail.com>
2025-11-30 10:49:40 +0300

    feat(helpers): add architecture-dependent constants

---
+++ helpers/calloc_64bit.go
@@ -0,0 +1,11 @@
+//go:build amd64 || arm64 || arm64be || ppc64 || ppc64le || mips64 || mips64le || riscv64 || s390x || sparc64
+// +build amd64 arm64 arm64be ppc64 ppc64le mips64 mips64le riscv64 s390x sparc64
+
+package helpers
+
+const (
+	// MaxArrayLen is a safe maximum length for slices on this architecture.
+	MaxArrayLen = 1<<50 - 1
+	// MaxBufferSize is the size of virtually unlimited buffer on this architecture.
+	MaxBufferSize = 256 << 30
+)
