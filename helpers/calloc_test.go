53935db30dcdf092cdc48e32ab378bc8ed53aac0
pchchv <ipchchv@gmail.com>
2025-11-30 11:03:00 +0300

    test(helpers): add benchmark for calloc

--- helpers/calloc_test.go
+++ helpers/calloc_test.go
@@ -2,7 +2,10 @@ package helpers
 
 import (
 	"fmt"
+	"math/rand"
+	"sync"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/require"
 )
@@ -35,4 +38,55 @@ func TestCalloc(t *testing.T) {
 	// Double free would panic when debug mode is enabled in jemalloc.
 	// Free(buf2)
 	// require.Equal(t, int64(0), NumAllocBytes())
+}
+
+// $ go test -failfast -run xxx -bench . -benchmem  -count 10 > out.txt
+// $ benchstat out.txt
+// name                 time/op
+// Allocation/Pool-8    200µs ± 5%
+// Allocation/Calloc-8  100µs ±11%
+//
+// name                 alloc/op
+// Allocation/Pool-8     477B ±29%
+// Allocation/Calloc-8  4.00B ± 0%
+//
+// name                 allocs/op
+// Allocation/Pool-8     1.00 ± 0%
+// Allocation/Calloc-8   0.00
+func BenchmarkAllocation(b *testing.B) {
+	b.Run("Pool", func(b *testing.B) {
+		pool := sync.Pool{
+			New: func() interface{} {
+				return make([]byte, 4<<10)
+			},
+		}
+		b.RunParallel(func(pb *testing.PB) {
+			source := rand.NewSource(time.Now().UnixNano())
+			r := rand.New(source)
+			for pb.Next() {
+				x := pool.Get().(*[]byte)
+				sz := r.Intn(100) << 10
+				if len(*x) < sz {
+					tempX := make([]byte, sz)
+					x = &tempX
+				}
+				r.Read(*x)
+				//nolint:staticcheck
+				pool.Put(x)
+			}
+		})
+	})
+
+	b.Run("Calloc", func(b *testing.B) {
+		b.RunParallel(func(pb *testing.PB) {
+			source := rand.NewSource(time.Now().UnixNano())
+			r := rand.New(source)
+			for pb.Next() {
+				sz := r.Intn(100) << 10
+				x := Calloc(sz, "test")
+				r.Read(x)
+				Free(x)
+			}
+		})
+	})
 }
