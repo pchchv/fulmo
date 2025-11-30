f36255185b42b065b76217f270036462f3059e4a
pchchv <ipchchv@gmail.com>
2025-11-30 11:20:24 +0300

    feat(sim): implement Parser type

--- sim/sim.go
+++ sim/sim.go
@@ -1,5 +1,15 @@
 package sim
 
+import (
+	"bufio"
+	"io"
+)
+
+
+// Parser is used as a parameter to NewReader,
+// allowing easy creation of Simulators from various trace file formats.
+type Parser func(string, error) ([]uint64, error)
+
 // Simulator is the core type of the `sim` package.
 // It is a function that returns a key from some source
 // (composed of other functions in this package, generated or parsed).
