8c091acd0430ebe4a263b23c40ebd14b9aa696ef
pchchv <ipchchv@gmail.com>
2025-11-30 11:24:24 +0300

    test(sim): add test for Zipfian constructor

--- sim/sim_test.go
+++ sim/sim_test.go
@@ -1,1 +1,19 @@
 package sim
+
+import "testing"
+
+func TestZipfian(t *testing.T) {
+	s := NewZipfian(1.5, 1, 100)
+	m := make(map[uint64]uint64, 100)
+	for i := 0; i < 100; i++ {
+		k, err := s()
+		if err != nil {
+			t.Fatal(err)
+		}
+		m[k]++
+	}
+
+	if len(m) == 0 || len(m) == 100 {
+		t.Fatal("zipfian not skewed")
+	}
+}
